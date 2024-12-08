12d43c5ce2ff73bebe217804c352ca26be05833e
LAHAN Tresor <haileystone0856@gmail.com>
2024-12-08 19:34:39 +0100

    ajust jobview link

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -19,7 +19,7 @@ const company = computed(() => state.job.company || {})
 
 onMounted(async () => {
     try {
-        const response = await axios.get('https://vue-jobs-mu.vercel.app/api/jobs');
+        const response = await axios.get(`https://vue-jobs-mu.vercel.app/api/jobs/${jobId}`);
         state.job = response.data;
     } catch (error) {
         console.error('Error fetching jobs', error)
