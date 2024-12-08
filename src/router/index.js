a9a4735793d9f808bb3587342fd9ec154704a01c
LAHAN Tresor <haileystone0856@gmail.com>
2024-12-08 19:51:35 +0100

    update route

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const router = createRouter({
             component: JobsView
         }, 
         {
-            path: '/jobs:id',
+            path: '/jobs/:id',
             name: 'job-details',
             component: JobView
         },
